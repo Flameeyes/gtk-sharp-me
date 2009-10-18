c8fe4d6974eb221860a7ff0582cd71d493d04935
Diego Elio 'Flameeyes' Pettenò <flameeyes@gmail.com>
2009-10-18 15:56:28 +0200

    Add a new generic CellRendererText child class.
    The new CellRendererTextDelegate<T> is a generic class that can work with a
    specified type (T) as property ("object"), and that calls a simple delegate
    to set up the Text and Weight properties as needed.

---
+++ Sources/CellRenderer.cs
@@ -0,0 +1,62 @@
+/*
+ * This file is part of GTK# Made Easy library.
+ * Copyright © 2009 Diego E. Pettenò <flameeyes@gmail.com>
+ *
+ * GTK# Made Easy is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * GTK# Made Easy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Portability.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+using System;
+using GLib;
+
+namespace GtkSharpME
+{
+	public class CellRendererTextDelegate<T>
+		: Gtk.CellRendererText
+	{
+		[Property("object")]
+		public T Object
+		{ get; set; }
+
+		public delegate void Setup(Gtk.CellRendererText cell, T obj);
+
+		protected Setup my_setup;
+
+		public CellRendererTextDelegate(Setup method)
+		{
+			my_setup = method;
+		}
+
+		public override void GetSize (Gtk.Widget widget, ref Gdk.Rectangle cell_area,
+		                              out int x_offset, out int y_offset,
+		                              out int width, out int height)
+		{
+			my_setup(this, Object);
+
+			base.GetSize (widget, ref cell_area,
+			              out x_offset, out y_offset,
+			              out width, out height);
+		}
+
+		protected override void Render (Gdk.Drawable window, Gtk.Widget widget,
+		                                Gdk.Rectangle background_area, Gdk.Rectangle cell_area,
+		                                Gdk.Rectangle expose_area, Gtk.CellRendererState flags)
+		{
+			my_setup(this, Object);
+
+			base.Render (window, widget, background_area, cell_area, expose_area, flags);
+		}
+	}
+}
